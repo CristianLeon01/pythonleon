a281c3adcc9a4efefe82a5f35eb49cc148e7f83f
cristian <leonvalenciacristiandavid@gmail.com>
2023-05-05 17:23:55 -0500

    Burbuja y Listas 05 - 05 - 23

---
+++ FUNCIONES/Miscelania de Ejercicios/listaRandom.py
@@ -0,0 +1,91 @@
+#Imprimir la suma, el promedio, el numero mayor, el numero menor...
+#La moda, la mediana y la desviacion extandar de una lista aleatoria.
+import math
+import random
+sum=0
+prom=0
+mayor=0
+menor=101
+moda=0
+resta=0
+division=0
+raiz=0
+moda=0
+mediana=0
+
+lista=[]
+tam=random.randint(10,20)
+print (f'En la lista hay {tam} elementos: ')
+for i in range (tam):
+    num=random.randrange(100)
+    lista.append(num)
+print(lista)
+print ()
+
+
+for numeros in lista:
+    sum += numeros
+print (f'La suma total es: {sum}')
+
+
+for numeros in lista:
+    prom = sum//tam
+print (f'El promedio es: {prom}')
+
+
+for numeros in lista:
+        if numeros>mayor:
+            mayor=numeros
+        if numeros<menor:
+             menor=numeros
+print(f'El numero mayor es: {mayor} ')
+print(f'El numero menor es: {menor} ')
+
+
+if tam % 2 == 0:
+     num1 = tam // 2
+     num2 = num1 -1
+     mediana = (lista[num1]+lista[num2]) // 2
+     print(f'La mediana es: {mediana}')
+else:
+    num_imp = tam // 2
+    mediana = (lista[num_imp])
+    print(f'La mediana es: {mediana}')
+
+
+max = 0
+for num in lista:
+    cont = 0
+    for s in lista:
+        if num == s:
+            cont +=1
+    if cont > max:
+        max = cont
+        moda =num
+print (f'La moda es: {moda}')
+
+
+for numeros in lista:
+     numeros = resta - (sum) / (prom)
+     cuadrado = resta ** 2
+     sum += cuadrado
+     division = sum / prom
+raiz = math.sqrt (division)
+print(f'La desviacion extandar es: {raiz} ')
+
+
+for i in range(tam-1):
+    for j in range(i+1,tam):
+        if lista[i]>lista[j]:
+            lista[i],lista[j]=lista[j],lista[i]
+print('El orden de la lista de menor a mayor es:')
+print (lista)
+print()
+
+
+for i in range(tam-1):
+    for j in range(i+1,tam):
+        if lista[i]<lista[j]:
+            lista[i],lista[j]=lista[j],lista[i]
+print('El orden de la lista de mayor a menor es: ')
+print (lista)
