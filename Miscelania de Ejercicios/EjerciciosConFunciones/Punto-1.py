f62f04e4c3a6e276075c7a1d05921a2284f11176
cristian <leonvalenciacristiandavid@gmail.com>
2023-05-10 17:17:58 -0500

    Version Funciones y Listas   10 - 05 - 23

--- Miscelania de Ejercicios/EjerciciosConFunciones/Punto-1.py
+++ Miscelania de Ejercicios/EjerciciosConFunciones/Punto-1.py
@@ -8,19 +8,41 @@ def llenarLista(tam,rango):
     lista=[random.randrange(rango) for i in range(tam)]
     return lista
 
+l1=llenarLista(tam,rango)
+
 def sumaLista(lista):
     sum=0
     for x in lista:
         sum+=x
     return sum
 
+
 def promedioLista(lista):
     return sumaLista(lista)/len(lista)
 
-l1=llenarLista(tam,rango)
+
+def numero_mayor(lista):
+    mayor = 0
+    for i in lista:
+        if i > mayor:
+            mayor = i
+    return mayor
+        
+
+def numero_menor(lista):
+    Menor = 1000000
+    for i in lista:
+        if i < Menor:
+            Menor = i
+    return Menor
+
 
 print(l1)
 
 print(sumaLista(l1))
 
 print(promedioLista(l1))
+
+print(numero_mayor(l1))
+
+print(numero_menor(l1))
