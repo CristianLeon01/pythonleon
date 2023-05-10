2a998fa3cfecfcfea5134b4e80b0586ef5f6c65c
cristian <leonvalenciacristiandavid@gmail.com>
2023-05-10 12:01:57 -0500

    Version Rebanadas y Comprensiones 10 - 05 - 23

--- FUNCIONES/Ejer1Comprensiones.py
+++ FUNCIONES/Ejer1Comprensiones.py
@@ -1,12 +1,11 @@
-import random
-import math
-
-tam = random.randint(5,10)
-lista = [random.randrange(100)for i in range(tam)]
-
-par = [i for i in lista if i %2 == 0 ]
-print(lista)
-
-
 #si el numero es par saquen la raiz cuadrada si en numero impar elevarlo al cuadradoo
+import random 
+import math 
 
+tam = random.randint(5, 10)
+
+lista =[random.randrange (100) for i in range (tam)]
+lista_2 = [round(math.sqrt(numero), 2) if numero % 2 == 0 else numero ** 2 for numero in lista]
+
+print("Lista original:", lista)
+print("Lista modificada:", lista_2)
--- FUNCIONES/Ejer1Rebanadas.py
+++ FUNCIONES/Ejer1Rebanadas.py
@@ -1,7 +1,43 @@
 import random
 import math
 
-lista = [random.random()*5 for i in range(20,30) ]
+lista = [round(random.random()*5,2) for i in range(20,30) ]
 print(lista)
-lista = round(lista,2)
+
+lista_completa=['Aprobados' if i > 3 else 'Suspendidos' for i in lista]
+lista_Apro=['Aprobados' for i in lista if i > 3 ]
+lista_Suspen=['Suspendido' for i in lista if i < 3 ]
+print()
+
+print(lista_completa)
+print()
+print(lista_Apro)
+print()
+print(lista_Suspen)
+print()
+
+lista.sort()
 print(lista)
+print()
+
+lista_0_1 = [x for x in lista if x < 2]
+print("Lista de Cero a Uno", lista_0_1)
+
+print()
+
+lista_2_3 = [x for x in lista if x > 2 and x < 4]
+print("Lista de Dos a Tres", lista_2_3)
+
+print()
+
+lista_4_5 = [x for x in lista if x > 4]
+print("Lista de Cuatro a Cinco", lista_4_5)
+
+lista_pro = [lista_Apro / len(lista_Apro)]
+print()
+print(lista_pro)
+
+
+
+
+
