1dc9cb7931045645020eb0912362ca44f2130173
cristian <leonvalenciacristiandavid@gmail.com>
2023-05-15 15:56:05 -0500

    Ruletazo V2

---
+++ Miscelania de Ejercicios/EjerciciosConFunciones/Punto-2.py
@@ -0,0 +1,79 @@
+import random
+
+def llenarLista(tam,rango):
+    lista=[]
+    lista=[random.randrange(rango) for i in range(tam)]
+    return lista
+
+l1 = llenarLista(10,40)
+l2 = llenarLista(10,40)
+
+print("La lista Numero 1 es la siguiente:",l1)
+print("La lista Numero 2 es la siguiente:",l2)
+print()
+
+def suma_lista(lista):
+    sum=0
+    for x in lista:
+        sum+=x
+    return sum
+
+print("La Suma Lista N1 es la siguiente:",suma_lista(l1))
+print("La Suma Lista N2 es la siguiente:",suma_lista(l2))
+print()
+
+def mayor_suma():
+    suma=0
+    if suma_lista(l1) > suma_lista(l2):
+        suma = "Lista 1", suma_lista(l1)
+    else:
+        suma = "Lista 2", suma_lista(l2)
+    return suma
+
+
+def num_mayor():
+    mayor = 0
+    for i in l1 and l2:
+        if i > mayor:
+            mayor = i
+    return mayor
+
+
+def num_menor():
+    menor = 100000
+    for i in l1 and l2:
+        if i < menor:
+            menor = i
+    return menor
+
+
+def prom_conjunto():
+    prom = (suma_lista(l1) + suma_lista(l2)) / 2
+    return prom
+
+
+def promedioLista(lista):
+    return suma_lista(lista)/len(lista)
+
+
+def pro_may_men():
+    if promedioLista(l1) > prom_conjunto():
+        print()
+
+
+
+print("El resultado mayor de la Suma de las dos Listas es la:", mayor_suma())
+print()
+print("El numero mayor de las 2 Listas es:",num_mayor())
+print("El numero menor de las 2 Listas es:",num_menor())
+print()
+print("El promedio del conjunto es:",prom_conjunto())
+print()
+print("El promedio de la lista N1 es:",promedioLista(l1))
+print("El promedio de la lista N2 es:",promedioLista(l2))
+print()
+
+
+
+
+
